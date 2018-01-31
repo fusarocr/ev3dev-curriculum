c5b9e58caa5b0627f80fa16c72200d78d3ad1418
backja <backja@rose-hulman.edu>
2018-01-31 15:15:27 -0500

    Finished m3

--- sandbox/m4/motors/m3_drive_inches_via_library.py
+++ sandbox/m4/motors/m3_drive_inches_via_library.py
@@ -61,7 +61,7 @@ def main():
             break
 
         robot.drive_inches(inches_target, speed_deg_per_second)
-        #ev3.Sound.beep().wait()  # Fun little beep
+        ev3.Sound.beep().wait()  # Fun little beep
 
     print("Goodbye!")
     ev3.Sound.speak("Goodbye").wait()
