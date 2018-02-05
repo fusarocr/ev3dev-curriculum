212d835246a193df5673943d4dc9a5bfdc5e511c
fusarocr <fusarocr@rose-hulman.edu>
2018-02-05 14:08:52 -0500

    reformat

--- sandbox/m4/digital_inputs/m6_ir_remote.py
+++ sandbox/m4/digital_inputs/m6_ir_remote.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
 """
-  This is your opportunity to work as a team on a mini-project.  You will need to handle the following actions.
+  This is your opportunity to work as a team on a mini-project.  You will need 
+  to handle the following actions.
 
-  When your program starts it should say IR Remote, print instructions, and do a robot.arm_calibration()
+  When your program starts it should say IR Remote, print instructions, and do
+   a robot.arm_calibration()
 
   IR remote channel 1 to drive the crawler tracks around:
-    -- Pressing  red up   makes the left  LED turn green and the left_motor  move at  600.
+    -- Pressing  red up   makes the left  LED turn green and the left_motor 
+     move at  600.
          Releasing turns off the LED and stops left_motor.
-    -- Pressing  red down makes the left  LED turn red   and the left_motor  move at -600.
+    -- Pressing  red down makes the left  LED turn red   and the left_motor  
+    move at -600.
          Releasing turns off the LED and stops left_motor.
-    -- Pressing  blue up  makes the right LED turn green and the right_motor move at  600.
+    -- Pressing  blue up  makes the right LED turn green and the right_motor 
+    move at  600.
          Releasing turns off the LED and stops right_motor.
-    -- Pressing blue down makes the right LED turn red   and the right_motor move at -600.
+    -- Pressing blue down makes the right LED turn red   and the right_motor 
+    move at -600.
          Releasing turns off the LED and stops right_motor.
-    You should be able to use one red (left) button and one blue (right) button at the same time.  For example:
-     while pressing both red up and blue down the robot should spin and LEDs will be green (left) and red (right).
+    You should be able to use one red (left) button and one blue (right) button 
+    at the same time.  For example:
+     while pressing both red up and blue down the robot should spin and LEDs
+      will be green (left) and red (right).
 
   IR remote channel 2 to raise and lower the arm
     -- Pressing red up   calls robot.arm_up().
@@ -22,8 +30,10 @@
     -- Pressing blue up  calls robot.arm_calibration().
 
   Buttons
-    -- Pressing the Back button will allow your program to end.  It should stop motors, turn on both green LEDs, and
-       then print and say Goodbye.  You will need to implement a new robot method called shutdown to handle this task.
+    -- Pressing the Back button will allow your program to end.  It should stop 
+    motors, turn on both green LEDs, and
+       then print and say Goodbye.  You will need to implement a new robot 
+       method called shutdown to handle this task.
 
 Authors: David Fisher and Jacob Back, Chris Fusaro, Craig McGee Jr.
 """  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
@@ -34,14 +44,17 @@ import time
 import robot_controller as robo
 
 
-# Note that todo2 is farther down in the code.  That method needs to be written before you do todo3.
+# Note that todo2 is farther down in the code.  That method needs to be written
+# before you do todo3.
 # Done: 3. Have someone on your team run this program on the EV3 and make sure
 # everyone understands the code.
-# Can you see what the robot does and explain what each line of code is doing? Talk as a group to make sure.
+# Can you see what the robot does and explain what each line of code is doing?
+# Talk as a group to make sure.
 
 
 class DataContainer(object):
-    """ Helper class that might be useful to communicate between different callbacks."""
+    """ Helper class that might be useful to communicate
+    between different callbacks."""
 
     def __init__(self):
         self.running = True
@@ -60,9 +73,12 @@ def main():
     robot = robo.Snatch3r()
     dc = DataContainer()
 
-    # TODO: 4. Add the necessary IR handler callbacks as per the instructions above.
-    # Remote control channel 1 is for driving the crawler tracks around (none of these functions exist yet below).
-    # Remote control channel 2 is for moving the arm up and down (all of these functions already exist below).
+    # Done: 4. Add the necessary IR handler callbacks as per the instructions
+    # above.
+    # Remote control channel 1 is for driving the crawler tracks around
+    # (none of these functions exist yet below).
+    # Remote control channel 2 is for moving the arm up and down
+    # (all of these functions already exist below).
     rc1 = ev3.RemoteControl(channel=1)
     rc2 = ev3.RemoteControl(channel=2)
     # For our standard shutdown button.
@@ -82,7 +98,7 @@ def main():
         button_state, robot)
     rc2.on_blue_up = lambda button_state: handle_calibrate_button(
         button_state, robot)
-    rc2.on_blue_down = lambda button_state: handle_shutdown(button_state,dc)
+    rc2.on_blue_down = lambda button_state: handle_shutdown(button_state, dc)
 
     robot.arm_calibration()  # Start with an arm calibration in this program.
 
@@ -95,10 +111,13 @@ def main():
 
     # Done: 2. Have everyone talk about this problem together then pick one
     # member to modify libs/robot_controller.py
-    # as necessary to implement the method below as per the instructions in the opening doc string. Once the code has
-    # been tested and shown to work, then have that person commit their work.  All other team members need to do a
+    # as necessary to implement the method below as per the instructions in the
+    #  opening doc string. Once the code has
+    # been tested and shown to work, then have that person commit their work.
+    #  All other team members need to do a
     # VCS --> Update project...
-    # Once the library is implemented any team member should be able to run his code as stated in todo3.
+    # Once the library is implemented any team member should be able to run his
+    # code as stated in todo3.
     robot.shutdown()
 
 
@@ -109,7 +128,8 @@ def main():
 # ----------------------------------------------------------------------
 # Done: 6. Implement the IR handler callbacks handlers.
 
-# TODO: 7. When your program is complete, call over a TA or instructor to sign your checkoff sheet and do a code review.
+# Done: 7. When your program is complete, call over a TA or instructor to sign
+# your checkoff sheet and do a code review.
 #
 # Observations you should make, IR buttons are a fun way to control the robot.
 
